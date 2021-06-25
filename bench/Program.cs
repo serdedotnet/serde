da6501b365946a128ce53fc6f6a88d00a2697c84
Andy Gocke <angocke@microsoft.com>
2021-06-24 19:21:58 -0700

    WIP

--- bench/Program.cs
+++ bench/Program.cs
@@ -1,8 +1,12 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using Benchmarks;
 
-var config = DefaultConfig.Instance.AddDiagnoser(MemoryDiagnoser.Default);
+var config = DefaultConfig.Instance
+    .AddDiagnoser(MemoryDiagnoser.Default)
+    .AddJob(Job.Default.WithRuntime(CoreRtRuntime.CoreRt50));
 var summary = BenchmarkSwitcher.FromAssembly(typeof(JsonToString<>).Assembly)
     .RunAll(config);
