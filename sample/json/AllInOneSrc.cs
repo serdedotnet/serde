9346d35e93d78eb538d6e44347cd8134e1356644
Andy Gocke <andy@commentout.net>
2022-05-10 20:58:29 -0700

    Add minimal sample

---
+++ sample/json/AllInOneSrc.cs
@@ -0,0 +1,70 @@
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Serde.Test
+{
+    [GenerateSerialize]
+    [GenerateDeserialize]
+    public sealed partial record AllInOne
+    {
+        public bool BoolField;
+        public char CharField;
+        public byte ByteField;
+        public ushort UShortField;
+        public uint UIntField;
+        public ulong ULongField;
+
+        public sbyte SByteField;
+        public short ShortField;
+        public int IntField;
+        public long LongField;
+
+        public string? StringField = null;
+
+        public int[]? IntArr = null;
+        public int[][]? NestedArr = null;
+
+        public ImmutableArray<int> IntImm;
+
+        public bool Equals(AllInOne? other)
+        {
+            return other is not null &&
+                BoolField == other.BoolField &&
+                CharField == other.CharField &&
+                ByteField == other.ByteField &&
+                UShortField == other.UShortField &&
+                UIntField == other.UIntField &&
+                ULongField == other.ULongField &&
+                SByteField == other.SByteField &&
+                ShortField == other.ShortField &&
+                IntField == other.IntField &&
+                LongField == other.LongField &&
+                StringField == other.StringField &&
+                IntArr.AsSpan().SequenceEqual(other.IntArr.AsSpan()) &&
+                NestedArr.AsSpan().SequenceEqual(other.NestedArr.AsSpan(),
+                    new Comparer()) &&
+                IntImm.AsSpan().SequenceEqual(other.IntImm.AsSpan());
+        }
+        private sealed class Comparer : IEqualityComparer<int[]>
+        {
+            public bool Equals(int[]? x, int[]? y)
+            {
+                return x?.AsSpan().SequenceEqual(y.AsSpan()) ?? y == null;
+            }
+
+            public int GetHashCode([DisallowNull] int[] obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ sample/json/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Serde.Json;
+using Serde.Test;
+
+var allInOne = new AllInOne();
+var json = JsonSerializer.Serialize(allInOne);
+Console.WriteLine(json);
+var second = JsonSerializer.Deserialize<AllInOne>(json);
+Console.Write(allInOne == second);
