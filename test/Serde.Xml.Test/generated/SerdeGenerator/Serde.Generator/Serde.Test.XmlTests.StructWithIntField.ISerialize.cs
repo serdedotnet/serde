344fc5ecb2af36c5602075ccdcaac1ddbb2b25e1
Andy Gocke <andy@commentout.net>
2023-04-04 07:52:01 -0700

    Move tests to use GeneratorDriver directly (#111)

---
+++ test/Serde.Xml.Test/generated/SerdeGenerator/Serde.Generator/Serde.Test.XmlTests.StructWithIntField.ISerialize.cs
@@ -0,0 +1,19 @@
+﻿
+#nullable enable
+using Serde;
+
+namespace Serde.Test
+{
+    partial class XmlTests
+    {
+        partial record StructWithIntField : Serde.ISerialize
+        {
+            void Serde.ISerialize.Serialize(ISerializer serializer)
+            {
+                var type = serializer.SerializeType("StructWithIntField", 1);
+                type.SerializeField("X", new Int32Wrap(this.X));
+                type.End();
+            }
+        }
+    }
+}
---
+++ test/Serde.Xml.Test/generated/SerdeGenerator/Serde.Generator/Serde.Test.XmlTests.TypeWithArrayField.ISerialize.cs
@@ -0,0 +1,19 @@
+﻿
+#nullable enable
+using Serde;
+
+namespace Serde.Test
+{
+    partial class XmlTests
+    {
+        partial class TypeWithArrayField : Serde.ISerialize
+        {
+            void Serde.ISerialize.Serialize(ISerializer serializer)
+            {
+                var type = serializer.SerializeType("TypeWithArrayField", 1);
+                type.SerializeField("ArrayField", new ArrayWrap.SerializeImpl<Serde.Test.XmlTests.StructWithIntField, IdWrap<Serde.Test.XmlTests.StructWithIntField>>(this.ArrayField));
+                type.End();
+            }
+        }
+    }
+}
