b3e5fc4eb35c7f04047f5d5b524a4ec96a7b04e6
Andy Gocke <andy@commentout.net>
2023-04-16 10:44:39 -0700

    Add MaxSizeType test

---
+++ test/Serde.Test/MaxSizeType.cs
@@ -0,0 +1,72 @@
+
+namespace Serde.Test;
+
+// Serde currently only supports types with <= 64 fields
+[GenerateSerde]
+internal readonly partial struct MaxSizeType
+{
+    public byte Field1 { get; init; }
+    public byte Field2 { get; init; }
+    public byte Field3 { get; init; }
+    public byte Field4 { get; init; }
+    public byte Field5 { get; init; }
+    public byte Field6 { get; init; }
+    public byte Field7 { get; init; }
+    public byte Field8 { get; init; }
+    public byte Field9 { get; init; }
+    public byte Field10 { get; init; }
+    public byte Field11 { get; init; }
+    public byte Field12 { get; init; }
+    public byte Field13 { get; init; }
+    public byte Field14 { get; init; }
+    public byte Field15 { get; init; }
+    public byte Field16 { get; init; }
+    public byte Field17 { get; init; }
+    public byte Field18 { get; init; }
+    public byte Field19 { get; init; }
+    public byte Field20 { get; init; }
+    public byte Field21 { get; init; }
+    public byte Field22 { get; init; }
+    public byte Field23 { get; init; }
+    public byte Field24 { get; init; }
+    public byte Field25 { get; init; }
+    public byte Field26 { get; init; }
+    public byte Field27 { get; init; }
+    public byte Field28 { get; init; }
+    public byte Field29 { get; init; }
+    public byte Field30 { get; init; }
+    public byte Field31 { get; init; }
+    public byte Field32 { get; init; }
+    public byte Field33 { get; init; }
+    public byte Field34 { get; init; }
+    public byte Field35 { get; init; }
+    public byte Field36 { get; init; }
+    public byte Field37 { get; init; }
+    public byte Field38 { get; init; }
+    public byte Field39 { get; init; }
+    public byte Field40 { get; init; }
+    public byte Field41 { get; init; }
+    public byte Field42 { get; init; }
+    public byte Field43 { get; init; }
+    public byte Field44 { get; init; }
+    public byte Field45 { get; init; }
+    public byte Field46 { get; init; }
+    public byte Field47 { get; init; }
+    public byte Field48 { get; init; }
+    public byte Field49 { get; init; }
+    public byte Field50 { get; init; }
+    public byte Field51 { get; init; }
+    public byte Field52 { get; init; }
+    public byte Field53 { get; init; }
+    public byte Field54 { get; init; }
+    public byte Field55 { get; init; }
+    public byte Field56 { get; init; }
+    public byte Field57 { get; init; }
+    public byte Field58 { get; init; }
+    public byte Field59 { get; init; }
+    public byte Field60 { get; init; }
+    public byte Field61 { get; init; }
+    public byte Field62 { get; init; }
+    public byte Field63 { get; init; }
+    public byte Field64 { get; init; }
+}
