447526c5c462a9f5c93dabfee84bdc37ff0817e3
Andy Gocke <andy@commentout.net>
2023-04-15 19:33:11 -0700

    Fix benchmarks (#122)

--- bench/Program.cs
+++ bench/Program.cs
@@ -1,8 +1,28 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Running;
 using Benchmarks;
 
+#if DEBUG
+
+const string LocationSample = DataGenerator.LocationSample;
+var options = new JsonSerializerOptions()
+{
+    IncludeFields = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+var loc1 = System.Text.Json.JsonSerializer.Deserialize<Location>(LocationSample, options);
+var loc2 = Serde.Json.JsonSerializer.Deserialize<Location>(LocationSample);
+
+Console.WriteLine(loc1 == loc2);
+
+#else
+
 var config = DefaultConfig.Instance.AddDiagnoser(MemoryDiagnoser.Default);
 var summary = BenchmarkSwitcher.FromAssembly(typeof(JsonToString<>).Assembly)
     .Run(args, config);
+
+#endif
