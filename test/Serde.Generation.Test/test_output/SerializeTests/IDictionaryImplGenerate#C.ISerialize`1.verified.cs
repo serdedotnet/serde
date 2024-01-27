d6043655f366ed4bd3ad4862a79e0ce99ecd516e
Andy Gocke <andy@commentout.net>
2024-01-27 14:51:56 -0800

    Refactor to allow adjusting the wrapped type

--- test/Serde.Generation.Test/test_output/SerializeTests/IDictionaryImplGenerate#C.ISerialize`1.verified.cs
+++ test/Serde.Generation.Test/test_output/SerializeTests/IDictionaryImplGenerate#C.ISerialize`1.verified.cs
@@ -9,7 +9,7 @@ partial class C : Serde.ISerialize<C>
     void ISerialize<C>.Serialize(C value, ISerializer serializer)
     {
         var type = serializer.SerializeType("C", 1);
-        type.SerializeField<R, Serde.IDictWrap.SerializeImpl<string, StringWrap, int, Int32Wrap>>("rDictionary", value.RDictionary);
+        type.SerializeField<System.Collections.Generic.IDictionary<string, int>, Serde.IDictWrap.SerializeImpl<string, StringWrap, int, Int32Wrap>>("rDictionary", value.RDictionary);
         type.End();
     }
 }
