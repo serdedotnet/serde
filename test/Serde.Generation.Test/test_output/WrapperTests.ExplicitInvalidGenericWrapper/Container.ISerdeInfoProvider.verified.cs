e63d3b5de03cbff9356cd51c570b088fe2548269
Andy Gocke <andy@commentout.net>
2024-07-28 21:36:40 -0700

    Provide an error if the explicit wrapper is bad

---
+++ test/Serde.Generation.Test/test_output/WrapperTests.ExplicitInvalidGenericWrapper/Container.ISerdeInfoProvider.verified.cs
@@ -0,0 +1,12 @@
+﻿//HintName: Container.ISerdeInfoProvider.cs
+
+#nullable enable
+partial record Container : Serde.ISerdeInfoProvider
+{
+    static global::Serde.ISerdeInfo global::Serde.ISerdeInfoProvider.SerdeInfo { get; } = Serde.SerdeInfo.MakeCustom(
+        "Container",
+        typeof(Container).GetCustomAttributesData(),
+        new (string, global::Serde.ISerdeInfo, System.Reflection.MemberInfo)[] {
+("sdkDir", global::Serde.SerdeInfoProvider.GetInfo<Proxy>(), typeof(Container).GetProperty("SdkDir")!)
+    });
+}
---
+++ test/Serde.Generation.Test/test_output/WrapperTests.ExplicitNullableProxy/Original.ISerdeInfoProvider.verified.cs
@@ -0,0 +1,12 @@
+﻿//HintName: Original.ISerdeInfoProvider.cs
+
+#nullable enable
+partial record struct Original : Serde.ISerdeInfoProvider
+{
+    static global::Serde.ISerdeInfo global::Serde.ISerdeInfoProvider.SerdeInfo { get; } = Serde.SerdeInfo.MakeCustom(
+        "Original",
+        typeof(Original).GetCustomAttributesData(),
+        new (string, global::Serde.ISerdeInfo, System.Reflection.MemberInfo)[] {
+("name", global::Serde.SerdeInfoProvider.GetInfo<global::Serde.StringWrap>(), typeof(Original).GetProperty("Name")!)
+    });
+}
