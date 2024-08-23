470014edd7c51746967089f71d4875b58e205592
Andy Gocke <andy@commentout.net>
2024-08-23 15:57:29 -0700

    Move to CPM (#192)

--- test/Serde.Generation.Test/GeneratorTestUtils.cs
+++ test/Serde.Generation.Test/GeneratorTestUtils.cs
@@ -15,7 +15,6 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing;
-using Microsoft.CodeAnalysis.Testing.Verifiers;
 using Microsoft.CodeAnalysis.Text;
 using VerifyTests;
 using VerifyXunit;
