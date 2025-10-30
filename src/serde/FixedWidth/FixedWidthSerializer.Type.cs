fc1d5e6245bc6580e36d83d8c71f980698c49671
Devin Duanne <devin.duanne@kclife.com>
2025-10-30 14:45:41 -0500

    Remove TextSerializer test

--- src/serde/FixedWidth/FixedWidthSerializer.Type.cs
+++ src/serde/FixedWidth/FixedWidthSerializer.Type.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace Serde.FixedWidth
 {
