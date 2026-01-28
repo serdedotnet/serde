d7edce6e69bae3d0a443ec8a86737c6d2c6330bd
Andy Gocke <andy@commentout.net>
2026-01-28 00:11:53 -0800

    Fix SerdeTypeOptions(Proxy =) and refactor
    SerdeTypeOptions wasn't be handled in a nested case, resulting in proxies being dropped.
    This change fixes that, and refactors the proxy handler to share more code, reducing the
    likelihood of this in the future.
    
    Fixes #300.

---
+++ test/Serde.Generation.Test/test_output/SerdeTests.DictionaryWithEqArrayKey/Test.ISerdeInfoProvider.g.verified.cs
@@ -0,0 +1,13 @@
+﻿//HintName: Test.ISerdeInfoProvider.g.cs
+
+#nullable enable
+partial class Test
+{
+    private static global::Serde.ISerdeInfo s_serdeInfo = Serde.SerdeInfo.MakeCustom(
+        "Test",
+    typeof(Test).GetCustomAttributesData(),
+    new (string, global::Serde.ISerdeInfo, System.Reflection.MemberInfo?)[] {
+        ("data", global::Serde.SerdeInfoProvider.GetSerializeInfo<System.Collections.Generic.Dictionary<EqArray<int>, int>, Serde.DictProxy.Ser<EqArray<int>, int, EqArrayProxy.Ser<int, global::Serde.I32Proxy>, global::Serde.I32Proxy>>(), typeof(Test).GetField("data"))
+    }
+    );
+}
---
+++ test/Serde.Generation.Test/test_output/SerdeTests.DictionaryWithEqArrayKey/Test.ISerdeProvider.g.verified.cs
@@ -0,0 +1,6 @@
+﻿//HintName: Test.ISerdeProvider.g.cs
+partial class Test : Serde.ISerdeProvider<Test, Test._SerdeObj, Test>
+{
+    static Test._SerdeObj global::Serde.ISerdeProvider<Test, Test._SerdeObj, Test>.Instance { get; }
+        = new Test._SerdeObj();
+}
