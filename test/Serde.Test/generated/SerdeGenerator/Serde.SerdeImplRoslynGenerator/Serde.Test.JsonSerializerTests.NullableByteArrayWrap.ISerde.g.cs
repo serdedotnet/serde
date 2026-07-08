5445bbe265a0fedb307a637944f510f7980ecf0f
Andy Gocke <andy@commentout.net>
2026-07-08 14:22:07 -0700

    Fix nullable arrays ser/de (#375)
    Nullable array serialization/deserialization wasn't correctly be wrapped
    in a NullablRefProxy due to a bug in the generator. The generator should
    now be fixed.

---
+++ test/Serde.Test/generated/SerdeGenerator/Serde.SerdeImplRoslynGenerator/Serde.Test.JsonSerializerTests.NullableByteArrayWrap.ISerde.g.cs
@@ -0,0 +1,67 @@
+
+#nullable enable
+
+using System;
+using Serde;
+
+namespace Serde.Test;
+
+partial class JsonSerializerTests
+{
+    partial record NullableByteArrayWrap
+    {
+        sealed partial class _SerdeObj : global::Serde.ISerde<Serde.Test.JsonSerializerTests.NullableByteArrayWrap>
+        {
+            global::Serde.ISerdeInfo global::Serde.ISerdeInfoProvider.SerdeInfo => Serde.Test.JsonSerializerTests.NullableByteArrayWrap.s_serdeInfo;
+
+            void global::Serde.ISerialize<Serde.Test.JsonSerializerTests.NullableByteArrayWrap>.Serialize(Serde.Test.JsonSerializerTests.NullableByteArrayWrap value, global::Serde.ISerializer serializer)
+            {
+                var _l_info = global::Serde.SerdeInfoProvider.GetInfo(this);
+                var _l_type = serializer.WriteType(_l_info);
+                _l_type.WriteValueIfNotNull<byte[], Serde.NullableRefProxy.Ser<byte[], global::Serde.ByteArrayProxy>>(_l_info, 0, value.Bytes);
+                _l_type.End(_l_info);
+            }
+            Serde.Test.JsonSerializerTests.NullableByteArrayWrap Serde.IDeserialize<Serde.Test.JsonSerializerTests.NullableByteArrayWrap>.Deserialize(IDeserializer deserializer)
+            {
+                byte[]? _l_bytes = default!;
+
+                byte _r_assignedValid = 0;
+
+                var _l_serdeInfo = global::Serde.SerdeInfoProvider.GetInfo(this);
+                var typeDeserialize = deserializer.ReadType(_l_serdeInfo);
+                while (true)
+                {
+                    var (_l_index_, _) = typeDeserialize.TryReadIndexWithName(_l_serdeInfo);
+                    if (_l_index_ == Serde.ITypeDeserializer.EndOfType)
+                    {
+                        break;
+                    }
+
+                    switch (_l_index_)
+                    {
+                        case 0:
+                            Serde.DeserializeException.ThrowIfDuplicate(_r_assignedValid, 0, _l_serdeInfo);
+                            _l_bytes = typeDeserialize.ReadValue<byte[]?, Serde.NullableRefProxy.De<byte[], global::Serde.ByteArrayProxy>>(_l_serdeInfo, _l_index_);
+                            _r_assignedValid |= ((byte)1) << 0;
+                            break;
+                        case Serde.ITypeDeserializer.IndexNotFound:
+                            typeDeserialize.SkipValue(_l_serdeInfo, _l_index_);
+                            break;
+                        default:
+                            throw new InvalidOperationException("Unexpected index: " + _l_index_);
+                    }
+                }
+                typeDeserialize.End(_l_serdeInfo);
+                if ((_r_assignedValid & 0b0) != 0b0)
+                {
+                    throw Serde.DeserializeException.UnassignedMember();
+                }
+                var newType = new Serde.Test.JsonSerializerTests.NullableByteArrayWrap() {
+                    Bytes = _l_bytes,
+                };
+
+                return newType;
+            }
+        }
+    }
+}
